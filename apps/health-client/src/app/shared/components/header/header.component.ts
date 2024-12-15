f9de14e45dac83cd37aa81897509d794cc9cb7d2
AlexLeoChumak <chumak1106@gmail.com>
2024-12-15 23:21:10 +0300

    add  RouterModule,     IonButtons,     ActionButtonComponent

--- apps/health-client/src/app/shared/components/header/header.component.ts
+++ apps/health-client/src/app/shared/components/header/header.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+} from '@ionic/angular/standalone';
+
+import { ActionButtonComponent } from 'src/app/shared/components/action-button/action-button.component';
 
 @Component({
   selector: 'health-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle],
+  imports: [
+    RouterModule,
+    IonButtons,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    ActionButtonComponent,
+  ],
 })
 export class HeaderComponent {}
