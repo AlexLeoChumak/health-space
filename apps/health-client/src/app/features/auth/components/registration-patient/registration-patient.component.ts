c009fd9c8332ad31f5c79eac5a2f324e9b806884
AlexLeoChumak <chumak1106@gmail.com>
2024-12-15 23:18:49 +0300

    add IonButtons, RouterModule

--- apps/health-client/src/app/features/auth/components/registration-patient/registration-patient.component.ts
+++ apps/health-client/src/app/features/auth/components/registration-patient/registration-patient.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import {
   IonHeader,
   IonToolbar,
@@ -12,6 +13,7 @@ import {
   IonGrid,
   IonRow,
   IonCol,
+  IonButtons,
 } from '@ionic/angular/standalone';
 
 import { RegistrationBaseComponent } from 'src/app/features/auth/components/registration-base/registration-base.component';
@@ -29,9 +31,11 @@ import { MobilePhoneNumberPasswordInfoFormComponent } from 'src/app/shared/compo
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
+    IonButtons,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    RouterModule,
     IonCol,
     IonRow,
     IonGrid,
