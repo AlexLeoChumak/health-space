ea6707a6dc425e19fb181f04fa52c6fa68665697
AlexLeoChumak <chumak1106@gmail.com>
2025-01-16 08:11:24 +0300

    add  path: '**'

--- apps/health-client/src/app/app.routes.ts
+++ apps/health-client/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { authRoutes } from 'src/app/features/auth/auth.routes';
+import { userProfileRoutes } from 'src/app/features/user-profile/user-profile.routes';
 
 export const routes: Routes = [
   {
@@ -8,9 +9,17 @@ export const routes: Routes = [
       import('./pages/home/home.page').then((m) => m.HomePage),
   },
   ...authRoutes,
+  ...userProfileRoutes,
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./core/components/not-found/not-found.component').then(
+        (m) => m.NotFoundComponent
+      ),
   },
 ];
