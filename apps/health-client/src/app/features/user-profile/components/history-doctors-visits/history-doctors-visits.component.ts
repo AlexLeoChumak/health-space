5a6879214967bb0ba57a1bcdf0b26b7bdbcfdf56
AlexLeoChumak <chumak1106@gmail.com>
2025-01-16 08:19:48 +0300

    create HistoryDoctorsVisitsComponent

---
+++ apps/health-client/src/app/features/user-profile/components/history-doctors-visits/history-doctors-visits.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'health-history-doctors-visits',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './history-doctors-visits.component.html',
+  styleUrl: './history-doctors-visits.component.scss',
+})
+export class HistoryDoctorsVisitsComponent {}
