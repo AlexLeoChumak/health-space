f787fb8a0793a4d366b455a378cba61d52c83815
AlexLeoChumak <chumak1106@gmail.com>
2025-01-16 08:13:00 +0300

    create LoaderComponent

---
+++ apps/health-client/src/app/core/components/loader/loader.component.ts
@@ -0,0 +1,36 @@
+import { Component, effect, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { LoadingController } from '@ionic/angular';
+import { selectIsLoading } from 'src/app/store/app';
+
+@Component({
+  selector: 'health-loader',
+  template: '',
+  standalone: true,
+})
+export class LoaderComponent {
+  private readonly store = inject(Store);
+  private readonly loadingCtrl = inject(LoadingController);
+  protected readonly isLoading = this.store.selectSignal(selectIsLoading);
+  private loaderInstance: HTMLIonLoadingElement | null = null;
+
+  constructor() {
+    effect(() => {
+      if (this.isLoading() && !this.loaderInstance) {
+        this.loadingCtrl
+          .create({
+            message: 'Пожалуйста, подождите...',
+            spinner: 'crescent',
+          })
+          .then((loader) => {
+            this.loaderInstance = loader;
+            loader.present();
+          });
+      } else if (!this.isLoading() && this.loaderInstance) {
+        this.loaderInstance.dismiss().then(() => {
+          this.loaderInstance = null;
+        });
+      }
+    });
+  }
+}
