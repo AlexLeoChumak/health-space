0d3f8985ae542cb8a14081d1167dfb824bad50c5
AlexLeoChumak <chumak1106@gmail.com>
2025-01-16 08:13:36 +0300

    create NotFoundComponent

---
+++ apps/health-client/src/app/core/components/not-found/not-found.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'health-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss',
+  standalone: true,
+  imports: [CommonModule],
+})
+export class NotFoundComponent {}
