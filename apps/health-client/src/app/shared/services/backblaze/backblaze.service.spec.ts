fa23a0e01fd49252b65c31442323d5326b41d509
AlexLeoChumak <chumak1106@gmail.com>
2025-01-19 16:22:11 +0300

    create BackblazeService

---
+++ apps/health-client/src/app/shared/services/backblaze/backblaze.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { BackblazeService } from './backblaze.service';
+
+describe('BackblazeService', () => {
+  let service: BackblazeService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(BackblazeService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
