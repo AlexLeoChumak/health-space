c63962e1cad6c9ff29c887220ed64aaac5e74ec1
AlexLeoChumak <chumak1106@gmail.com>
2025-01-20 22:18:47 +0300

    move logic from component to ngrx effect

--- apps/health-client/src/app/features/user-profile/components/user-profile/user-profile.component.ts
+++ apps/health-client/src/app/features/user-profile/components/user-profile/user-profile.component.ts
@@ -1,11 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  OnDestroy,
-  OnInit,
-  signal,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   IonHeader,
@@ -22,14 +15,12 @@ import {
   IonToolbar,
   IonButton,
 } from '@ionic/angular/standalone';
-import { LetDirective } from '@ngrx/component';
 import { Store } from '@ngrx/store';
 
-import { UserState, selectUser } from 'src/app/store/user';
+import { UserState, selectUrlUserPhoto, selectUser } from 'src/app/store/user';
 import { logout } from 'src/app/store/app';
-import { BackblazeService } from 'src/app/shared/services/backblaze/backblaze.service';
-import { filter, Subject, switchMap, takeUntil } from 'rxjs';
-import { GlobalApiSuccessResponseInterface } from 'src/app/shared/models/global-api-success-response.interface';
+
+// import { LetDirective } from '@ngrx/component'; для ngrxLet и ngrxPush
 
 @Component({
   selector: 'health-user-profile',
@@ -52,48 +43,14 @@ import { GlobalApiSuccessResponseInterface } from 'src/app/shared/models/global-
     IonLabel,
     IonAvatar,
     IonHeader,
-    LetDirective,
   ],
 })
-export class UserProfileComponent implements OnInit, OnDestroy {
+export class UserProfileComponent {
   private readonly store = inject(Store<UserState>);
-  private readonly backblazeService = inject(BackblazeService);
-  protected readonly user$ = this.store.select(selectUser);
-  private readonly defaultProfileImage = 'default-profile-image.png';
-  protected readonly userPhotoUrl = signal(this.defaultProfileImage);
-  private readonly destroy$ = new Subject<void>();
-
-  public ngOnInit(): void {
-    this.initializeUserInfo();
-  }
-
-  private initializeUserInfo() {
-    this.user$
-      .pipe(
-        filter(Boolean),
-        switchMap((user) => {
-          const fileName = user?.personalInfo?.photo as string;
-
-          return this.backblazeService.authorize().pipe(
-            switchMap(() => {
-              return this.backblazeService.getPrivatePhotoUrl(fileName);
-            })
-          );
-        }),
-        takeUntil(this.destroy$)
-      )
-      .subscribe((response: GlobalApiSuccessResponseInterface<string>) => {
-        this.userPhotoUrl.set(response.data);
-      });
-  }
+  protected readonly user = this.store.selectSignal(selectUser);
+  protected readonly urlUserPhoto = this.store.selectSignal(selectUrlUserPhoto);
 
   protected onLogout() {
     this.store.dispatch(logout());
-    this.userPhotoUrl.set(this.defaultProfileImage);
-  }
-
-  public ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
   }
 }
