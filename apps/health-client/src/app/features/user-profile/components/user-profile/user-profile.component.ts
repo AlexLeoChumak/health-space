11ac49833e0f0d216d6937493cba78af2e32c825
AlexLeoChumak <chumak1106@gmail.com>
2025-01-31 16:56:27 +0300

    add ActionButtonComponent

--- apps/health-client/src/app/features/user-profile/components/user-profile/user-profile.component.ts
+++ apps/health-client/src/app/features/user-profile/components/user-profile/user-profile.component.ts
@@ -1,7 +1,6 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
-  IonHeader,
   IonAvatar,
   IonLabel,
   IonItem,
@@ -11,16 +10,17 @@ import {
   IonCardContent,
   IonList,
   IonText,
-  IonTitle,
-  IonToolbar,
-  IonButton,
+  IonImg,
+  IonCol,
+  IonGrid,
+  IonRow,
+  IonFooter,
 } from '@ionic/angular/standalone';
 import { Store } from '@ngrx/store';
 
-import { UserState, selectUrlUserPhoto, selectUser } from 'src/app/store/user';
+import { selectUrlUserPhoto, selectUser } from 'src/app/store/user';
 import { logout } from 'src/app/store/app';
-
-// import { LetDirective } from '@ngrx/component'; для ngrxLet и ngrxPush
+import { ActionButtonComponent } from 'src/app/shared/components/action-button/action-button.component';
 
 @Component({
   selector: 'health-user-profile',
@@ -29,24 +29,26 @@ import { logout } from 'src/app/store/app';
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
+    IonRow,
+    IonGrid,
+    IonCol,
+    IonImg,
     CommonModule,
-    IonButton,
-    IonToolbar,
-    IonTitle,
     IonText,
     IonList,
     IonCardContent,
     IonContent,
     IonCardHeader,
+    IonFooter,
     IonCard,
     IonItem,
     IonLabel,
     IonAvatar,
-    IonHeader,
+    ActionButtonComponent,
   ],
 })
 export class UserProfileComponent {
-  private readonly store = inject(Store<UserState>);
+  private readonly store = inject(Store);
   protected readonly user = this.store.selectSignal(selectUser);
   protected readonly urlUserPhoto = this.store.selectSignal(selectUrlUserPhoto);
 
