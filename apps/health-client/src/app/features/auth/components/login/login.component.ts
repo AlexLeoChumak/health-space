39730e1901b62b566c1854ad09018c306e46e0c8
AlexLeoChumak <chumak1106@gmail.com>
2025-01-31 16:55:36 +0300

    add isSubmittingForm

--- apps/health-client/src/app/features/auth/components/login/login.component.ts
+++ apps/health-client/src/app/features/auth/components/login/login.component.ts
@@ -1,6 +1,13 @@
-import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import {
   IonHeader,
@@ -19,15 +26,20 @@ import { Store } from '@ngrx/store';
 
 import { LoginRequestInterface } from 'src/app/shared/models/login-request.interface';
 import { ActionButtonComponent } from 'src/app/shared/components/action-button/action-button.component';
-import { selectUser } from 'src/app/store/user';
 import { MobilePhoneNumberPasswordInfoFormComponent } from 'src/app/shared/components/mobile-phone-number-password-info-form/mobile-phone-number-password-info-form.component';
-import { AppState, login } from 'src/app/store/app';
+import {
+  AppState,
+  login,
+  selectIsAuthenticated,
+  selectIsLoading,
+} from 'src/app/store/app';
 
 @Component({
   selector: 'health-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     IonButtons,
     RouterModule,
@@ -48,30 +60,19 @@ import { AppState, login } from 'src/app/store/app';
   ],
 })
 export class LoginComponent implements OnInit {
-  private readonly router = inject(Router);
   private readonly store = inject(Store<AppState>);
   private readonly destroyRef = inject(DestroyRef);
   public loginForm!: FormGroup;
   protected readonly isDoctor = signal<boolean>(false);
+  protected readonly isSubmittingForm =
+    this.store.selectSignal(selectIsLoading);
 
   public ngOnInit(): void {
     this.initializeForm();
-    this.subscribeToUserState();
   }
 
   private initializeForm(): void {
     this.loginForm = new FormGroup({});
-  }
-
-  private subscribeToUserState(): void {
-    this.store
-      .select(selectUser)
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((user) => {
-        if (user) {
-          this.router.navigate(['/user-profile']);
-        }
-      });
   }
 
   protected onSubmitForm(): void {
@@ -80,6 +81,19 @@ export class LoginComponent implements OnInit {
     const loginData: LoginRequestInterface = this.loginForm.value;
 
     this.store.dispatch(login({ loginData, isDoctor: this.isDoctor() }));
+
+    this.store
+      .select(selectIsAuthenticated)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (success) => {
+          if (success) {
+            this.loginForm.reset({
+              mobilePhoneNumber: '+375',
+            });
+          }
+        },
+      });
   }
 
   protected addFormGroup(formGroup: FormGroup): void {
