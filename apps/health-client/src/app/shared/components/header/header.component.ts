cd1d990a0fa2e50df147675e40c02c679f8f9758
AlexLeoChumak <chumak1106@gmail.com>
2025-01-31 16:57:55 +0300

    add protected readonly isAuthenticated

--- apps/health-client/src/app/shared/components/header/header.component.ts
+++ apps/health-client/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {
   IonHeader,
@@ -6,14 +6,17 @@ import {
   IonTitle,
   IonButtons,
 } from '@ionic/angular/standalone';
+import { Store } from '@ngrx/store';
 
 import { ActionButtonComponent } from 'src/app/shared/components/action-button/action-button.component';
+import { selectIsAuthenticated } from 'src/app/store/app';
 
 @Component({
   selector: 'health-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     RouterModule,
     IonButtons,
@@ -23,4 +26,9 @@ import { ActionButtonComponent } from 'src/app/shared/components/action-button/a
     ActionButtonComponent,
   ],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private readonly store = inject(Store);
+  protected readonly isAuthenticated = this.store.selectSignal(
+    selectIsAuthenticated
+  );
+}
