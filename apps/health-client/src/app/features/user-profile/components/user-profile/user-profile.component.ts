3fe9678eccc59e6bbf737b4d722bb1a12b9831ba
AlexLeoChumak <chumak1106@gmail.com>
2025-02-02 16:02:51 +0300

    moved the markup into a separate component

--- apps/health-client/src/app/features/user-profile/components/user-profile/user-profile.component.ts
+++ apps/health-client/src/app/features/user-profile/components/user-profile/user-profile.component.ts
@@ -5,29 +5,15 @@ import {
   inject,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  IonAvatar,
-  IonLabel,
-  IonItem,
-  IonCard,
-  IonCardHeader,
-  IonContent,
-  IonCardContent,
-  IonList,
-  IonText,
-  IonImg,
-  IonCol,
-  IonGrid,
-  IonRow,
-  IonCardTitle,
-} from '@ionic/angular/standalone';
+import { IonContent, IonCol, IonGrid, IonRow } from '@ionic/angular/standalone';
 import { Store } from '@ngrx/store';
 
 import { logout } from 'src/app/store/app';
-import { selectUrlUserPhoto, selectUser } from 'src/app/store/user';
+import { selectUser } from 'src/app/store/user';
 import { ActionButtonComponent } from 'src/app/shared/components/action-button/action-button.component';
 import { AddressInfoCardComponent } from 'src/app/shared/components/cards-user-info/address-info-card/address-info-card.component';
 import { getUserRole } from 'src/app/shared/utils/get-user-role.utility';
+import { PersonalInfoCardComponent } from 'src/app/shared/components/cards-user-info/personal-info-card/personal-info-card.component';
 
 @Component({
   selector: 'health-user-profile',
@@ -36,29 +22,19 @@ import { getUserRole } from 'src/app/shared/utils/get-user-role.utility';
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
-    IonCardTitle,
     IonRow,
     IonGrid,
     IonCol,
-    IonImg,
     CommonModule,
-    IonText,
-    IonList,
-    IonCardContent,
     IonContent,
-    IonCardHeader,
-    IonCard,
-    IonItem,
-    IonLabel,
-    IonAvatar,
     ActionButtonComponent,
     AddressInfoCardComponent,
+    PersonalInfoCardComponent,
   ],
 })
 export class UserProfileComponent {
   private readonly store = inject(Store);
   protected readonly user = this.store.selectSignal(selectUser);
-  protected readonly urlUserPhoto = this.store.selectSignal(selectUrlUserPhoto);
   protected readonly userRole = computed(() => {
     const currentUser = this.user();
     return currentUser ? getUserRole(currentUser) : null;
