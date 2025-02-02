c9381251df27d8c880954bc256736edf65332083
AlexLeoChumak <chumak1106@gmail.com>
2025-02-02 15:29:38 +0300

    change path

--- apps/health-client/src/app/features/auth/components/registration-patient/registration-patient.component.ts
+++ apps/health-client/src/app/features/auth/components/registration-patient/registration-patient.component.ts
@@ -18,11 +18,12 @@ import {
 
 import { RegistrationBaseComponent } from 'src/app/features/auth/components/registration-base/registration-base.component';
 import { ActionButtonComponent } from 'src/app/shared/components/action-button/action-button.component';
-import { AddressInfoFormComponent } from 'src/app/shared/components/address-info-form/address-info-form.component';
-import { ContactInfoFormComponent } from 'src/app/shared/components/contact-info-form/contact-info-form.component';
-import { IdentificationInfoFormComponent } from 'src/app/shared/components/identification-info-form/identification-info-form.component';
-import { PersonalInfoFormComponent } from 'src/app/shared/components/personal-info-form/personal-info-form.component';
-import { MobilePhoneNumberPasswordInfoFormComponent } from 'src/app/shared/components/mobile-phone-number-password-info-form/mobile-phone-number-password-info-form.component';
+
+import { PersonalInfoFormComponent } from 'src/app/shared/components/forms-user-info/personal-info-form/personal-info-form.component';
+import { MobilePhoneNumberPasswordInfoFormComponent } from 'src/app/shared/components/forms-user-info/mobile-phone-number-password-info-form/mobile-phone-number-password-info-form.component';
+import { AddressInfoFormComponent } from 'src/app/shared/components/forms-user-info/address-info-form/address-info-form.component';
+import { ContactInfoFormComponent } from 'src/app/shared/components/forms-user-info/contact-info-form/contact-info-form.component';
+import { IdentificationInfoFormComponent } from 'src/app/shared/components/forms-user-info/identification-info-form/identification-info-form.component';
 
 @Component({
   selector: 'health-registration-patient',
