de7635e2a039291ecf7d3f2325bf4741dd888607
AlexLeoChumak <chumak1106@gmail.com>
2025-02-04 13:38:19 +0300

    add changeDetection: ChangeDetectionStrategy.OnPush

--- apps/health-client/src/app/shared/components/footer/footer.component.ts
+++ apps/health-client/src/app/shared/components/footer/footer.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-import { IonFooter, IonToolbar, IonTitle } from '@ionic/angular/standalone';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { IonToolbar, IonTitle, IonFooter } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'health-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [IonFooter, IonToolbar, IonTitle],
 })
 export class FooterComponent {}
