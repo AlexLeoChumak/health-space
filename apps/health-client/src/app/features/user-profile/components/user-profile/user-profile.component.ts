df94a6e154b9a51f2bd7640d89c6be4556fc0743
AlexLeoChumak <chumak1106@gmail.com>
2025-02-04 13:36:06 +0300

    add ContactInfoCardComponent,     FooterComponent

--- apps/health-client/src/app/features/user-profile/components/user-profile/user-profile.component.ts
+++ apps/health-client/src/app/features/user-profile/components/user-profile/user-profile.component.ts
@@ -12,9 +12,11 @@ import { logout } from 'src/app/store/app';
 import { selectUser } from 'src/app/store/user';
 import { ActionButtonComponent } from 'src/app/shared/components/action-button/action-button.component';
 import { AddressInfoCardComponent } from 'src/app/shared/components/cards-user-info/address-info-card/address-info-card.component';
-import { getUserRole } from 'src/app/shared/utils/get-user-role.utility';
+import { getUserRole } from 'src/app/shared/utilities/get-user-role.utility';
 import { PersonalInfoCardComponent } from 'src/app/shared/components/cards-user-info/personal-info-card/personal-info-card.component';
 import { IdentificationInfoCardComponent } from 'src/app/shared/components/cards-user-info/identification-info-card/identification-info-card.component';
+import { ContactInfoCardComponent } from 'src/app/shared/components/cards-user-info/contact-info-card/contact-info-card.component';
+import { FooterComponent } from 'src/app/shared/components/footer/footer.component';
 
 @Component({
   selector: 'health-user-profile',
@@ -32,6 +34,8 @@ import { IdentificationInfoCardComponent } from 'src/app/shared/components/cards
     AddressInfoCardComponent,
     PersonalInfoCardComponent,
     IdentificationInfoCardComponent,
+    ContactInfoCardComponent,
+    FooterComponent,
   ],
 })
 export class UserProfileComponent {
