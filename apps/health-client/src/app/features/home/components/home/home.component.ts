7259bea21cd0a66aa3bafabec67b11639c33c1fa
AlexLeoChumak <chumak1106@gmail.com>
2025-02-05 17:28:14 +0300

    create HomeComponent

---
+++ apps/health-client/src/app/features/home/components/home/home.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonContent } from '@ionic/angular/standalone';
+import { FooterComponent } from 'src/app/shared/components/footer/footer.component';
+
+@Component({
+  selector: 'health-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, IonContent, FooterComponent],
+})
+export class HomeComponent {}
