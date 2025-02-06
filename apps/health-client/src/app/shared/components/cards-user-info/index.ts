420130e9740ed2c9333d47a89ebb1a7ddc857c87
AlexLeoChumak <chumak1106@gmail.com>
2025-02-06 21:14:33 +0300

    create barrel file

---
+++ apps/health-client/src/app/shared/components/cards-user-info/index.ts
@@ -0,0 +1,6 @@
+export * from './address-info-card/address-info-card.component';
+export * from './contact-info-card/contact-info-card.component';
+export * from './education-medical-worker-info-card/education-medical-worker-info-card.component';
+export * from './identification-info-card/identification-info-card.component';
+export * from './personal-info-card/personal-info-card.component';
+export * from './place-work-info-card/place-work-info-card.component';
