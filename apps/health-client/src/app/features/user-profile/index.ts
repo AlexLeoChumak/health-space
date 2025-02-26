dffac010f81542a8924bd30cde3ec391ce039f57
AlexLeoChumak <chumak1106@gmail.com>
2025-02-26 16:02:30 +0300

    add export * from './components/edit-profile/edit-profile.component';

--- apps/health-client/src/app/features/user-profile/index.ts
+++ apps/health-client/src/app/features/user-profile/index.ts
@@ -1,2 +1,4 @@
 export * from './user-profile.routes';
 export * from './components/user-profile/user-profile.component';
+export * from './components/edit-profile/edit-profile.component';
+export * from './models/child-obj-child-name.interface';
