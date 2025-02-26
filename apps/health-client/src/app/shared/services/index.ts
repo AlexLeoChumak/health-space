1a53d72d467efb5d6bc5325529a961ddef13d43f
AlexLeoChumak <chumak1106@gmail.com>
2025-02-26 16:03:44 +0300

    add export * from './string-converter/string-converter.service';

--- apps/health-client/src/app/shared/services/index.ts
+++ apps/health-client/src/app/shared/services/index.ts
@@ -2,3 +2,4 @@ export * from './backblaze/backblaze.service';
 export * from './local-storage/local-storage.service';
 export * from './toast/toast.service';
 export * from './user-photo/user-photo.service';
+export * from './string-converter/string-converter.service';
