add23254ad0fb96c58ce8740ac61831dcc21cf1c
AlexLeoChumak <chumak1106@gmail.com>
2025-02-27 23:20:36 +0300

    create getIdUserSection, setUserSectionData

--- apps/health-client/src/app/store/user/user.reducer.ts
+++ apps/health-client/src/app/store/user/user.reducer.ts
@@ -3,8 +3,10 @@ import { createReducer, on } from '@ngrx/store';
 import { getUserRole } from 'src/app/shared/utilities/get-user-role.utility';
 import {
   clearUser,
+  getIdUserSection,
   getUrlUserPhotoSuccess,
   loadUser,
+  setUserSectionData,
 } from 'src/app/store/user/user.actions';
 import { initialUserState } from 'src/app/store/user/user.state';
 
@@ -18,6 +20,14 @@ export const userReducer = createReducer(
   on(getUrlUserPhotoSuccess, (state, { urlUserPhoto }) => ({
     ...state,
     urlUserPhoto,
+  })),
+  on(getIdUserSection, (state, { idUserSection }) => ({
+    ...state,
+    idUserSection,
+  })),
+  on(setUserSectionData, (state, { userSectionData }) => ({
+    ...state,
+    userSectionData,
   })),
   on(clearUser, () => initialUserState)
 );
