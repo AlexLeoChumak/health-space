bf83b874c4668a35dbb2d67472d221a2b70fa2d0
AlexLeoChumak <chumak1106@gmail.com>
2025-02-27 23:22:00 +0300

    add idUserSection, userSectionData

--- apps/health-client/src/app/store/user/user.state.ts
+++ apps/health-client/src/app/store/user/user.state.ts
@@ -1,3 +1,4 @@
+import { ChildObjChildNameInterface } from 'src/app/shared/models';
 import { DoctorInterface } from 'src/app/shared/models/doctor/doctor.interface';
 import { PatientInterface } from 'src/app/shared/models/patient/patient.interface';
 
@@ -5,10 +6,14 @@ export interface UserState {
   user: PatientInterface | DoctorInterface | null;
   role: 'patient' | 'doctor' | null;
   urlUserPhoto: string;
+  idUserSection: string | null;
+  userSectionData: ChildObjChildNameInterface | null;
 }
 
 export const initialUserState: UserState = {
   user: null,
   role: null,
   urlUserPhoto: 'default-profile-image.png',
+  idUserSection: null,
+  userSectionData: null,
 };
