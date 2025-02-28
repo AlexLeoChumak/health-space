555875948f36b830af4f0b7e26d31fac6fbbacb5
AlexLeoChumak <chumak1106@gmail.com>
2025-02-28 12:51:31 +0300

    remove export * from './models/child-obj-child-name.interface';

--- apps/health-client/src/app/features/user-profile/index.ts
+++ apps/health-client/src/app/features/user-profile/index.ts
@@ -1,4 +1,3 @@
 export * from './user-profile.routes';
 export * from './components/user-profile/user-profile.component';
 export * from './components/edit-profile/edit-profile.component';
-export * from './models/child-obj-child-name.interface';
