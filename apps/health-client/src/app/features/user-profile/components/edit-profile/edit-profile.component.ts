24f6c6bfad241883f181113c8b635689d679888c
AlexLeoChumak <chumak1106@gmail.com>
2025-02-28 12:52:46 +0300

    create EditProfileComponent

---
+++ apps/health-client/src/app/features/user-profile/components/edit-profile/edit-profile.component.ts
@@ -0,0 +1,111 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+import { Store } from '@ngrx/store';
+import {
+  IonCard,
+  IonContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonButton,
+  IonIcon,
+  IonToolbar,
+  IonButtons,
+} from '@ionic/angular/standalone';
+
+import { setIdUserSection } from 'src/app/store/user';
+import {
+  AddressInfoFormComponent,
+  ContactInfoFormComponent,
+  EducationMedicalWorkerInfoFormComponent,
+  IdentificationBelarusCitizenInfoFormComponent,
+  IdentificationForeignCitizenInfoFormComponent,
+  PersonalInfoFormComponent,
+  PlaceWorkInfoFormComponent,
+} from 'src/app/shared/components';
+
+@Component({
+  selector: 'health-edit-profile',
+  templateUrl: './edit-profile.component.html',
+  styleUrl: './edit-profile.component.scss',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    IonButtons,
+    IonToolbar,
+    ReactiveFormsModule,
+    IonIcon,
+    IonButton,
+    IonCardContent,
+    IonCardTitle,
+    IonCardHeader,
+    IonContent,
+    IonCard,
+    CommonModule,
+    AddressInfoFormComponent,
+    ContactInfoFormComponent,
+    EducationMedicalWorkerInfoFormComponent,
+    PersonalInfoFormComponent,
+    PlaceWorkInfoFormComponent,
+    IdentificationBelarusCitizenInfoFormComponent,
+    IdentificationForeignCitizenInfoFormComponent,
+  ],
+})
+export class EditProfileComponent implements OnInit {
+  private readonly store = inject(Store);
+  private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+  protected readonly editProfileForm = new FormGroup({});
+  private readonly destroyRef = inject(DestroyRef);
+
+  readonly section = toSignal(
+    this.route.queryParams.pipe(map((params) => params['section'] || null))
+  );
+
+  public ngOnInit(): void {
+    this.dispatchSectionIdFromQueryParams();
+  }
+
+  private dispatchSectionIdFromQueryParams(): void {
+    this.route.queryParams
+      .pipe(
+        map((params) => params['section-id'] || null),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe((idUserSection: string) => {
+        this.store.dispatch(setIdUserSection({ idUserSection }));
+      });
+  }
+
+  protected addFormGroup(formGroupName: string, formGroup: FormGroup): void {
+    Object.keys(this.editProfileForm.controls).forEach((control) =>
+      this.editProfileForm.removeControl(control)
+    );
+
+    this.editProfileForm.addControl(formGroupName, formGroup);
+  }
+
+  protected onSubmitEditedForm(): void {
+    if (this.editProfileForm.valid) {
+      const updatedData = this.editProfileForm.value;
+      console.log('Данные для сохранения:', updatedData);
+      // Здесь можно отправить данные на сервер для сохранения
+    } else {
+      console.log('Форма содержит ошибки');
+    }
+  }
+
+  protected goToUserProfilePage(): void {
+    this.router.navigate(['/user-profile']);
+  }
+}
