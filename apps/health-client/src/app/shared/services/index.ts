013b300cdaf7ba5e8de504fad32d0025c5610c54
AlexLeoChumak <chumak1106@gmail.com>
2025-03-04 23:04:15 +0300

    add export * from './cloud-storage/cloud-storage.service';

--- apps/health-client/src/app/shared/services/index.ts
+++ apps/health-client/src/app/shared/services/index.ts
@@ -1,6 +1,5 @@
-export * from './backblaze/backblaze.service';
 export * from './local-storage/local-storage.service';
 export * from './toast/toast.service';
-export * from './user-photo/user-photo.service';
 export * from './string-converter/string-converter.service';
 export * from './navigation/navigation.service';
+export * from './cloud-storage/cloud-storage.service';
