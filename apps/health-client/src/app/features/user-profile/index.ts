eb967974ece972dd0db6ff70cdba827f6e99058e
AlexLeoChumak <chumak1106@gmail.com>
2025-03-05 22:46:34 +0300

    add export * from './models/update-password.interface'; export * from './constants/password.constant';

--- apps/health-client/src/app/features/user-profile/index.ts
+++ apps/health-client/src/app/features/user-profile/index.ts
@@ -2,3 +2,5 @@ export * from './user-profile.routes';
 export * from './components/user-profile/user-profile.component';
 export * from './components/edit-profile/edit-profile.component';
 export * from './components/change-password-form/change-password-form.component';
+export * from './models/update-password.interface';
+export * from './constants/password.constant';
