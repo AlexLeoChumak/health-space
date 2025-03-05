4363c44e6722e28b3b90458c6adc9212dadaf678
AlexLeoChumak <chumak1106@gmail.com>
2025-03-05 22:45:41 +0300

    create PASSWORD_CONSTANT

---
+++ apps/health-client/src/app/features/user-profile/constants/password.constant.ts
@@ -0,0 +1,8 @@
+export interface PasswordConstantInterface {
+  passwordsNoMatch: string;
+}
+
+export const PASSWORD_CONSTANT: PasswordConstantInterface = {
+  passwordsNoMatch:
+    'Новый пароль и его подтверждение не совпадают. Проверьте их и попробуйте еще раз.',
+};
