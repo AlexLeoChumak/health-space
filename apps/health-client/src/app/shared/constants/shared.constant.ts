9c66fc2ffb74a2ccd73ca5a8127768dc1d99c220
AlexLeoChumak <chumak1106@gmail.com>
2025-03-08 19:36:47 +0300

    create SHARED_CONSTANT

---
+++ apps/health-client/src/app/shared/constants/shared.constant.ts
@@ -0,0 +1,7 @@
+export interface SharedConstantInterface {
+  USER_NOT_FOUND_ERROR: string;
+}
+
+export const SHARED_CONSTANT: SharedConstantInterface = {
+  USER_NOT_FOUND_ERROR: 'Пользователь не найден',
+};
