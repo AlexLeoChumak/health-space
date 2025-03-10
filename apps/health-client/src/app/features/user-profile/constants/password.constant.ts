c2275624d7ef92a046847dfed6ec7c90118fcf93
AlexLeoChumak <chumak1106@gmail.com>
2025-03-10 20:44:55 +0300

    remove

--- apps/health-client/src/app/features/user-profile/constants/password.constant.ts
+++
@@ -1,8 +0,0 @@
-export interface PasswordConstantInterface {
-  NO_MATCH: string;
-}
-
-export const PASSWORD_CONSTANT: PasswordConstantInterface = {
-  NO_MATCH:
-    'Новый пароль и его подтверждение не совпадают. Проверьте их и попробуйте еще раз.',
-};
