1a77fb88d707b7f95571de7a2a524e8e5cbfdd44
AlexLeoChumak <chumak1106@gmail.com>
2025-03-11 21:20:27 +0300

    create authGuard

--- apps/health-client/src/app/core/guards/auth/auth.guard.ts
+++ apps/health-client/src/app/core/guards/auth/auth.guard.ts
@@ -1,32 +1,8 @@
-import { DestroyRef, inject } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { CanActivateFn, Router } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { filter, map, switchMap } from 'rxjs';
-
-import {
-  selectIsAppInitialized,
-  selectIsAuthenticated,
-} from 'src/app/store/app';
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { AuthService } from 'src/app/features/auth/services/auth/auth.service';
 
 export const authGuard: CanActivateFn = () => {
-  const store = inject(Store);
-  const router = inject(Router);
-  const destroyRef = inject(DestroyRef);
-
-  return store.select(selectIsAppInitialized).pipe(
-    filter((isInitialized) => isInitialized), // Ждем, пока приложение завершит инициализацию
-    switchMap(() =>
-      store.select(selectIsAuthenticated).pipe(
-        map((isAuthenticated) => {
-          if (isAuthenticated) {
-            router.navigate(['/']); // Если залогинен, перенаправляем на главную
-            return false;
-          }
-          return true; // Если не залогинен, разрешаем доступ
-        })
-      )
-    ),
-    takeUntilDestroyed(destroyRef) // Один раз для всей цепочки
-  );
+  const authService = inject(AuthService);
+  return !!authService.getToken();
 };
