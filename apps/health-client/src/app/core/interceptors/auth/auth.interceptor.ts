a7caab5e8c34e599b91e559747e9581263b925ef
AlexLeoChumak <chumak1106@gmail.com>
2025-03-11 21:24:50 +0300

    create authInterceptor

--- apps/health-client/src/app/core/interceptors/auth/auth.interceptor.ts
+++ apps/health-client/src/app/core/interceptors/auth/auth.interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { AuthService } from 'src/app/features/auth';
+import { AuthService } from 'src/app/features/auth/services/auth/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
