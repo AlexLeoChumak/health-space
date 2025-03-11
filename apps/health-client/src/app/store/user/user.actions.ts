7b383818e1e6646489975f9b2469d86f36295ed1
AlexLeoChumak <chumak1106@gmail.com>
2025-03-11 21:26:47 +0300

    create loadUserSuccess, loadUserFailure

--- apps/health-client/src/app/store/user/user.actions.ts
+++ apps/health-client/src/app/store/user/user.actions.ts
@@ -1,13 +1,21 @@
 import { createAction, props } from '@ngrx/store';
-import { ChildObjChildNameInterface } from 'src/app/shared/models';
+import {
+  ChildObjChildNameInterface,
+  ErrorMessageInterface,
+} from 'src/app/shared/models';
 import { DoctorInterface } from 'src/app/shared/models/doctor/doctor.interface';
 import { PatientInterface } from 'src/app/shared/models/patient/patient.interface';
 
-export const loadUser = createAction(
-  '[User] Load User',
-  props<{
-    user: PatientInterface | DoctorInterface;
-  }>()
+export const loadUser = createAction('[User] Load User');
+
+export const loadUserSuccess = createAction(
+  '[User] Load User Success',
+  props<{ user: PatientInterface | DoctorInterface }>()
+);
+
+export const loadUserFailure = createAction(
+  '[User] Load User Failure',
+  props<ErrorMessageInterface>()
 );
 
 export const setUrlUserPhotoSuccess = createAction(
