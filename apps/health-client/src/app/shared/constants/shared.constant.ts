aa8bcedb2d83a0556aebcc103a12e598bffbec34
AlexLeoChumak <chumak1106@gmail.com>
2025-03-14 21:36:55 +0300

    add UPDATE_USER_PROFILE_IMAGE_ERROR

--- apps/health-client/src/app/shared/constants/shared.constant.ts
+++ apps/health-client/src/app/shared/constants/shared.constant.ts
@@ -1,7 +1,9 @@
 export interface SharedConstantInterface {
   USER_NOT_FOUND_ERROR: string;
+  UPDATE_USER_PROFILE_IMAGE_ERROR: string;
 }
 
 export const SHARED_CONSTANT: SharedConstantInterface = {
   USER_NOT_FOUND_ERROR: 'Пользователь не найден',
+  UPDATE_USER_PROFILE_IMAGE_ERROR: 'Ошибка обновления изображения',
 };
