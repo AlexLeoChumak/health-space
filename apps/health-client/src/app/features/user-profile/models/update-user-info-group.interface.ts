9fbac6154e31fbe22ec80ac919c1fd775161f7fd
AlexLeoChumak <chumak1106@gmail.com>
2025-03-14 21:37:33 +0300

    remove

--- apps/health-client/src/app/features/user-profile/models/update-user-info-group.interface.ts
+++
@@ -1,8 +0,0 @@
-import { UserRoleType } from 'src/app/features/user-profile';
-import { UpdateUserInfoGroupType } from 'src/app/features/user-profile/models/update-user-info-group.type';
-
-export interface UpdateUserInfoGroupInterface {
-  updateInfoGroup: UpdateUserInfoGroupType;
-  userId: string;
-  userRole: UserRoleType;
-}
