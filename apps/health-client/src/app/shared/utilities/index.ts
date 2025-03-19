cafaa71796bb1538451ebaf827f73ae5a7b95a57
AlexLeoChumak <chumak1106@gmail.com>
2025-03-19 20:11:21 +0300

    add export * from './deep-equal.util';

--- apps/health-client/src/app/shared/utilities/index.ts
+++ apps/health-client/src/app/shared/utilities/index.ts
@@ -2,3 +2,4 @@ export * from './check-input-validator.utility';
 export * from './formatting-date-to-local-string.utility';
 export * from './get-datepicker-button-label.utility';
 export * from './get-user-role.utility';
+export * from './deep-equal.util';
