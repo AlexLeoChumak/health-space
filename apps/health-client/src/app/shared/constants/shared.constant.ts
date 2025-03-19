78f20e9acf2ea76e407893cfa4a36e302827df71
AlexLeoChumak <chumak1106@gmail.com>
2025-03-19 20:09:39 +0300

    add UPDATE_INFO_SUCCESSFULLY: string;

--- apps/health-client/src/app/shared/constants/shared.constant.ts
+++ apps/health-client/src/app/shared/constants/shared.constant.ts
@@ -1,9 +1,11 @@
 export interface SharedConstantInterface {
   USER_NOT_FOUND_ERROR: string;
+  UPDATE_INFO_SUCCESSFULLY: string;
   UPDATE_USER_PROFILE_IMAGE_ERROR: string;
 }
 
 export const SHARED_CONSTANT: SharedConstantInterface = {
   USER_NOT_FOUND_ERROR: 'Пользователь не найден',
+  UPDATE_INFO_SUCCESSFULLY: 'Информация успешно обновлёна',
   UPDATE_USER_PROFILE_IMAGE_ERROR: 'Ошибка обновления изображения',
 };
