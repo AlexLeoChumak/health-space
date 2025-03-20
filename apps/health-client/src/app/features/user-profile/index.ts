e69e17e66c08b5330289f1e30655b1b81e94c3cd
AlexLeoChumak <chumak1106@gmail.com>
2025-03-20 20:48:31 +0300

    rename to update-profile

--- apps/health-client/src/app/features/user-profile/index.ts
+++ apps/health-client/src/app/features/user-profile/index.ts
@@ -1,6 +1,6 @@
 export * from './user-profile.routes';
 export * from './components/user-profile/user-profile.component';
-export * from './components/edit-profile/edit-profile.component';
+export * from './components/update-profile/update-profile.component';
 export * from './components/update-password-form/update-password-form.component';
 export * from './models/update-password.interface';
 export * from './constants/update-info.constant';
