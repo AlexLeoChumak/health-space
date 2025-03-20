e4191c2088c2d4f474985d5c4833b519aa5fd7d8
AlexLeoChumak <chumak1106@gmail.com>
2025-03-20 21:14:53 +0300

    remove  on(appInitialize

--- apps/health-client/src/app/store/app/app.reducer.ts
+++ apps/health-client/src/app/store/app/app.reducer.ts
@@ -1,7 +1,5 @@
 import { createReducer, on } from '@ngrx/store';
 import {
-  appInitialize,
-  appInitialized,
   initialAppState,
   login,
   loginFailure,
@@ -12,36 +10,24 @@ import {
 
 export const appReducer = createReducer(
   initialAppState,
-  on(appInitialize, (state) => ({
-    ...state,
-    isLoading: true,
-  })),
-  on(appInitialized, (state) => ({
-    ...state,
-    isAppInitialized: true,
-    isLoading: false,
-  })),
   on(setLoading, (state, { isLoading }) => ({
     ...state,
     isLoading,
   })),
   on(login, (state) => ({
     ...state,
-    isLoading: true,
     error: null,
   })),
   on(loginSuccess, (state, { accessToken }) => ({
     ...state,
     isAuthenticated: true,
     accessToken,
-    isLoading: false,
     error: null,
   })),
   on(loginFailure, (state, { message }) => ({
     ...state,
     isAuthenticated: false,
     accessToken: null,
-    isLoading: false,
     error: message,
   })),
   on(logout, (state) => ({
