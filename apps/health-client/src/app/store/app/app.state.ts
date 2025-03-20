b0620751e00522ea20253df7694156161a5e326b
AlexLeoChumak <chumak1106@gmail.com>
2025-03-20 21:15:44 +0300

    remove   isAppInitialized: boolean

--- apps/health-client/src/app/store/app/app.state.ts
+++ apps/health-client/src/app/store/app/app.state.ts
@@ -1,15 +1,13 @@
 export interface AppState {
-  isAppInitialized: boolean; // Флаг, показывающий, инициализировано ли приложение
   isLoading: boolean; // Глобальный флаг загрузки
-  isAuthenticated: boolean; // Флаг авторизации пользователя
+  isAuthenticated: boolean | null; // Флаг авторизации пользователя
   accessToken: string | null; // Токен доступа пользователя
   error: string | null; // Ошибка (например, при логине)
 }
 
 export const initialAppState: AppState = {
-  isAppInitialized: false,
   isLoading: false,
-  isAuthenticated: false,
+  isAuthenticated: null,
   accessToken: null,
   error: null,
 };
