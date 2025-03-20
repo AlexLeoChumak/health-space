3d967a68f7f400078d933d41535ba8d93b891201
AlexLeoChumak <chumak1106@gmail.com>
2025-03-20 21:13:33 +0300

    create index.ts

---
+++ apps/health-client/src/app/shared/utils/index.ts
@@ -0,0 +1,5 @@
+export * from './check-input-validator.util';
+export * from './formatting-date-to-local-string.util';
+export * from './get-datepicker-button-label.util';
+export * from './get-user-role.util';
+export * from './deep-equal.util';
