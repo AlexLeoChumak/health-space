77bec162943f1f840f1506cbad2df3d5a726c6bf
AlexLeoChumak <chumak1106@gmail.com>
2025-03-21 10:39:07 +0300

    add return value type - boolean

--- apps/health-client/src/app/core/guards/auth/auth.guard.ts
+++ apps/health-client/src/app/core/guards/auth/auth.guard.ts
@@ -2,7 +2,7 @@ import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AuthService } from 'src/app/features/auth/services/auth/auth.service';
 
-export const authGuard: CanActivateFn = () => {
+export const authGuard: CanActivateFn = (): boolean => {
   const authService = inject(AuthService);
   return !!authService.getToken();
 };
