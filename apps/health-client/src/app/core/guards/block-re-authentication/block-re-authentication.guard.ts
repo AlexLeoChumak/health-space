381324c886249801bcafe76df662d5f3e0d0d17b
AlexLeoChumak <chumak1106@gmail.com>
2025-03-21 10:39:26 +0300

    add return value type - Observable<boolean>

--- apps/health-client/src/app/core/guards/block-re-authentication/block-re-authentication.guard.ts
+++ apps/health-client/src/app/core/guards/block-re-authentication/block-re-authentication.guard.ts
@@ -1,24 +1,22 @@
-import { inject, DestroyRef } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { tap, map, take, filter } from 'rxjs';
+import { tap, map, take, filter, Observable } from 'rxjs';
 import { selectIsAuthenticated } from 'src/app/store/app';
 
-export const blockReAuthenticationGuard: CanActivateFn = () => {
-  const store = inject(Store);
-  const router = inject(Router);
-  const destroyRef = inject(DestroyRef);
+export const blockReAuthenticationGuard: CanActivateFn =
+  (): Observable<boolean> => {
+    const store = inject(Store);
+    const router = inject(Router);
 
-  return store.select(selectIsAuthenticated).pipe(
-    filter((isAuthenticated) => isAuthenticated !== null),
-    take(1),
-    tap((isAuthenticated) => {
-      if (isAuthenticated) {
-        router.navigate(['/']);
-      }
-    }),
-    map((isAuthenticated) => !isAuthenticated),
-    takeUntilDestroyed(destroyRef)
-  );
-};
+    return store.select(selectIsAuthenticated).pipe(
+      filter((isAuthenticated) => isAuthenticated !== null),
+      take(1),
+      tap((isAuthenticated) => {
+        if (isAuthenticated) {
+          router.navigate(['/']);
+        }
+      }),
+      map((isAuthenticated) => !isAuthenticated)
+    );
+  };
