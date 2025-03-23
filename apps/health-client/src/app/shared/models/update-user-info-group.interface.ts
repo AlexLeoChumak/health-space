ce0c3498fd040bf9bf3ae3f615faea092c7e9d18
AlexLeoChumak <chumak1106@gmail.com>
2025-03-23 12:10:20 +0300

    add currentUserPhotoData: CurrentUserPhotoDataInterface;

--- apps/health-client/src/app/shared/models/update-user-info-group.interface.ts
+++ apps/health-client/src/app/shared/models/update-user-info-group.interface.ts
@@ -1,7 +1,9 @@
 import { UpdateUserInfoGroupType, UserRoleType } from 'src/app/shared/models';
+import { CurrentUserPhotoDataInterface } from 'src/app/shared/models/current-user-photo-data.interface';
 
 export interface UpdateUserInfoGroupInterface {
   updateInfoGroup: UpdateUserInfoGroupType;
   userId: string;
   userRole: UserRoleType;
+  currentUserPhotoData: CurrentUserPhotoDataInterface;
 }
