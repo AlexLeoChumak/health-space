e664f34de3afcd29f81d6d657ee45cae02a2c22c
AlexLeoChumak <chumak1106@gmail.com>
2025-03-29 13:52:40 +0300

    create  GeneralSettingUserProfileComponent

--- apps/health-client/src/app/features/user-profile/components/general-setting-user-profile/general-setting-user-profile.component.ts
+++ apps/health-client/src/app/features/user-profile/components/general-setting-user-profile/general-setting-user-profile.component.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'health-general-setting-user-profile',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './general-setting-user-profile.component.html',
   styleUrl: './general-setting-user-profile.component.scss',
+  standalone: true,
+  imports: [CommonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GeneralSettingUserProfileComponent {}
