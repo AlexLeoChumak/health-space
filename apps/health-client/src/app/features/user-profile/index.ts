161e15f6eac49aec6827498e5994f80d4842675e
AlexLeoChumak <chumak1106@gmail.com>
2025-04-07 21:59:34 +0300

    add verify-password.interface

--- apps/health-client/src/app/features/user-profile/index.ts
+++ apps/health-client/src/app/features/user-profile/index.ts
@@ -3,6 +3,7 @@ export * from './components/user-profile/user-profile.component';
 export * from './components/update-user-profile/update-user-profile.component';
 export * from './components/update-password-form/update-password-form.component';
 export * from './models/update-password.interface';
+export * from './models/verify-password.interface';
 export * from './constants/update-info.constant';
 export * from './components/setting-user-profile/setting-user-profile.component';
 export * from './components/remove-user-profile/remove-user-profile.component';
