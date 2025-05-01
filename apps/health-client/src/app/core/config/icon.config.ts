41040e923f7879dd129ba4991d7851cfa6fbcfbe
AlexLeoChumak <chumak1106@gmail.com>
2025-05-01 19:03:54 +0300

    create configureIcons

---
+++ apps/health-client/src/app/core/config/icon.config.ts
@@ -0,0 +1,40 @@
+import { addIcons } from 'ionicons';
+import {
+  home,
+  logIn,
+  menu,
+  pencil,
+  person,
+  personAdd,
+  save,
+  trashBin,
+  arrowBack,
+  eyeOff,
+  eye,
+  logOutOutline,
+  settings,
+  personCircle,
+  settingsOutline,
+  trashOutline,
+} from 'ionicons/icons';
+
+export const configureIcons = () => {
+  addIcons({
+    person,
+    home,
+    trashBin,
+    personAdd,
+    logIn,
+    menu,
+    pencil,
+    save,
+    arrowBack,
+    eyeOff,
+    eye,
+    logOutOutline,
+    settings,
+    personCircle,
+    settingsOutline,
+    trashOutline,
+  });
+};
