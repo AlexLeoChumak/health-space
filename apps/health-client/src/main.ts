7032e3e14c05112e9496f9b5cdb17a1cd060e316
AlexLeoChumak <chumak1106@gmail.com>
2025-05-01 19:03:01 +0300

    removed unnecessary logic from the file

--- apps/health-client/src/main.ts
+++ apps/health-client/src/main.ts
@@ -1,85 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import {
-  RouteReuseStrategy,
-  provideRouter,
-  withPreloading,
-  PreloadAllModules,
-} from '@angular/router';
-import {
-  IonicRouteStrategy,
-  provideIonicAngular,
-} from '@ionic/angular/standalone';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideStore } from '@ngrx/store';
-import { provideEffects } from '@ngrx/effects';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { addIcons } from 'ionicons';
-import {
-  home,
-  logIn,
-  menu,
-  pencil,
-  person,
-  personAdd,
-  save,
-  trashBin,
-  arrowBack,
-  eyeOff,
-  eye,
-  logOutOutline,
-  settings,
-  personCircle,
-  settingsOutline,
-  trashOutline,
-} from 'ionicons/icons';
-
-import * as RegistrationEffects from 'src/app/store/registration/registration.effects';
-import * as AppEffects from 'src/app/store/app/app.effects';
-import * as UserEffects from 'src/app/store/user/user.effects';
 import { AppComponent } from 'src/app/app.component';
-import { routes } from 'src/app/app.routes';
-import { authInterceptor, errorInterceptor } from 'src/app/core/interceptors';
-import { appReducer } from 'src/app/store/app';
-import { registrationReducer } from 'src/app/store/registration';
-import { userReducer } from 'src/app/store/user';
+import { appConfig } from 'src/app/app.config';
+import { configureIcons } from 'src/app/core/config/icon.config';
 
-addIcons({
-  person,
-  home,
-  trashBin,
-  personAdd,
-  logIn,
-  menu,
-  pencil,
-  save,
-  arrowBack,
-  eyeOff,
-  eye,
-  logOutOutline,
-  settings,
-  personCircle,
-  settingsOutline,
-  trashOutline,
-});
+configureIcons();
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
-    provideStore({
-      app: appReducer,
-      registration: registrationReducer,
-      user: userReducer,
-    }),
-    provideEffects(AppEffects, RegistrationEffects, UserEffects),
-    provideStoreDevtools(),
-  ],
-});
-
-// nx g @nx/angular:service shared/services/backblaze/backblaze --project=health-client
-// nx g @nx/angular:component apps/health-client/src/app/shared/components/cards-user-info/address-info-card/address-info-card
-// nx g @nx/angular:component apps/health-client/src/app/features/user-profile/components/general-setting-user-profile/general-setting-user-profile
-
-// import { LetDirective } from '@ngrx/component'; для ngrxLet и ngrxPush
+bootstrapApplication(AppComponent, appConfig);
